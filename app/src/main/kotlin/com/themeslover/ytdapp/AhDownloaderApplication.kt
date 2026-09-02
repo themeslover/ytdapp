7d637851086d3376744d8670557528c0132d1769
themeslover <malikpyary@gmail.com>
2026-09-02 17:12:02 +0500

    feat: request Android local network permission automatically

---
+++ app/src/main/kotlin/com/themeslover/ytdapp/AhDownloaderApplication.kt
@@ -0,0 +1,33 @@
+package com.themeslover.ytdapp
+
+import android.Manifest
+import android.app.Activity
+import android.app.Application
+import android.os.Build
+import android.os.Bundle
+
+class AhDownloaderApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityResumed(activity: Activity) {
+                if (Build.VERSION.SDK_INT >= 37 &&
+                    checkSelfPermission(Manifest.permission.ACCESS_LOCAL_NETWORK) != android.content.pm.PackageManager.PERMISSION_GRANTED
+                ) {
+                    activity.requestPermissions(arrayOf(Manifest.permission.ACCESS_LOCAL_NETWORK), REQUEST_LOCAL_NETWORK)
+                }
+            }
+
+            override fun onActivityCreated(activity: Activity, state: Bundle?) = Unit
+            override fun onActivityStarted(activity: Activity) = Unit
+            override fun onActivityPaused(activity: Activity) = Unit
+            override fun onActivityStopped(activity: Activity) = Unit
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+            override fun onActivityDestroyed(activity: Activity) = Unit
+        })
+    }
+
+    companion object {
+        private const val REQUEST_LOCAL_NETWORK = 7001
+    }
+}
